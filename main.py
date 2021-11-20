181abf6ef3bac161e3bff98f8c3fd11432fcf891
educatedadi <93342026+educatedadi@users.noreply.github.com>
2021-11-20 13:33:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 from player_bat import PlayerBat
 from screen_menu import CreateScreen
 from ball import Ball
+from scoreboard import ScoreBoard
 
 canvas = Screen()
 
@@ -15,6 +16,7 @@ create_screen = CreateScreen()
 right_side_bat = PlayerBat((350, 0))
 left_side_bat = PlayerBat((-350, 0))
 my_ball = Ball()
+score = ScoreBoard()
 
 
 def switch_off():
@@ -36,8 +38,19 @@ while game_is_on:
     if my_ball.ycor() > 290 or my_ball.ycor() < -290:
         my_ball.bounce()
 
-    if my_ball.distance(right_side_bat) < 50:
-        my_ball.bounce()
+    if my_ball.xcor() > 320 or my_ball.xcor() < -320:
+        if my_ball.distance(right_side_bat) < 50 or my_ball.distance(left_side_bat) < 50:
+            my_ball.hit_by_bat()
+
+    # Detects if right side misses the ball
+    if my_ball.xcor() > 400:
+        my_ball.reset_position()
+        score.l_increase()
+
+    # Detects if left side misses the ball
+    if my_ball.xcor() < -400:
+        my_ball.reset_position()
+        score.r_increase()
 
     canvas.onkey(switch_off, 'e')
 
