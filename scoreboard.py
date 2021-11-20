e5ace2bfa07f8b28f85f570b55aaa685cf40e368
educatedadi <93342026+educatedadi@users.noreply.github.com>
2021-11-20 13:39:27 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,1 +1,30 @@
 
+from turtle import Turtle
+SCORE_FONT = ('Chunq', 72, 'bold')
+
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color('white')
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update()
+
+    def update(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, False, 'center', SCORE_FONT)
+        self.goto(100, 200)
+        self.write(self.r_score, False, 'center', SCORE_FONT)
+
+    def l_increase(self):
+        self.l_score += 1
+        self.update()
+
+    def r_increase(self):
+        self.r_score += 1
+        self.update()
+
