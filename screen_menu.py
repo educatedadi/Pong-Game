79786830209da12dc5d07c315b069fe849f5af0f
educatedadi <93342026+educatedadi@users.noreply.github.com>
2021-11-20 13:40:30 +0530

    Update screen_menu.py

--- screen_menu.py
+++ screen_menu.py
@@ -2,26 +2,32 @@ from turtle import Turtle
 
 POSITIONS = [(-420, 300), (420, 300), (420, -295), (-420, -295), (-420, 300)]
 MIDDLE_BORDER_POS = [(0, 310), (0, -310)]
+GAME_OVER_FONT = ('Chunq', 24, 'bold')
 
 
-class CreateScreen:
+class CreateScreen(Turtle):
     def __init__(self):
-        self.border = Turtle()
-        self.border.color('white')
-        self.border.hideturtle()
-        self.border.speed('fastest')
-        self.border.pensize(20)
-        self.border.penup()
+        super().__init__()
+        self.color('white')
+        self.hideturtle()
+        self.speed('fastest')
+        self.pensize(20)
+        self.penup()
         for coordinates in POSITIONS:
-            self.border.goto(coordinates)
-            self.border.pendown()
-        self.border.penup()
-        self.border.pensize(5)
-        self.border.goto(MIDDLE_BORDER_POS[0])
-        self.border.setheading(270)
-        while 320 > self.border.ycor() > -320:
-            self.border.forward(10)
-            self.border.penup()
-            self.border.forward(10)
-            self.border.pendown()
+            self.goto(coordinates)
+            self.pendown()
+        self.penup()
+        self.pensize(5)
+        self.goto(MIDDLE_BORDER_POS[0])
+        self.setheading(270)
+        while 320 > self.ycor() > -320:
+            self.forward(10)
+            self.penup()
+            self.forward(10)
+            self.pendown()
+        self.penup()
+
+    def game_over(self):
+        self.home()
+        self.write('G A M E  O V E R !', False, align='center', font=GAME_OVER_FONT)
 
